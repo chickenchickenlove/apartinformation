69261ad0f81f525030d72542b6ae0e56f0e0864b
ChickenchickenLove <ojt90902@naver.com>
2022-03-19 09:47:04 +0900

    Add files via upload

---
+++ src/main/java/apartinformation/apart/ApartApplication.java
@@ -0,0 +1,13 @@
+package apartinformation.apart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartApplication.class, args);
+	}
+
+}
